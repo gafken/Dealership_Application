212adc76adb52076c84ffe81e48445ba58ba0364
gafken <rtd91@live.com>
2015-07-27 10:58:30 -0400

    Initial Blank MVC upload
    -

---
+++ Dealship App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dealship_App.Startup))]
+namespace Dealship_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
