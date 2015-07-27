be862501abb89ac1b067ab1bfa8743d739755195
gafken <rtd91@live.com>
2015-07-27 15:59:53 -0400

    Vehicle Model
    Added model

--- Dealship App/Models/IdentityModels.cs
+++ Dealship App/Models/IdentityModels.cs
@@ -9,6 +9,18 @@ namespace Dealship_App.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public bool crudAccess
+        {
+            get
+            {
+                return this.crudAccess;
+            }
+            set
+            {
+                this.crudAccess = value;
+            }
+        }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
